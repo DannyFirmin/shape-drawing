5e1f5792ff0a542a42ffb2a4cee16628a586cfc9
Danny Feng <u6611178@anu.edu.au>
2018-03-21 00:43:30 +1100

    Finishing Warmup

--- src/Warmup.hs
+++ src/Warmup.hs
@@ -3,6 +3,18 @@
 --
 
 module Main where
+import CodeWorld
 
+myPicture :: Picture
+myPicture = colored red (translated 3.5 3.5 (solidRectangle 2.5 2.5))
+
+yourPicture :: Picture
+yourPicture = translated 3 3 (circle 2)
+
+hisPicture :: Picture
+hisPicture = translated (-3) 6 (rectangle 4 4)
+
+ourPicture :: Picture
+ourPicture = myPicture & yourPicture & hisPicture
 main :: IO ()
-main = putStrLn "Hello world!"
+main = drawingOf ourPicture
