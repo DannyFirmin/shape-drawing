c1e517438b0d3a67ef070a08a5bdd33e423797a8
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-04-10 10:01:22 +1000

    Add feedback comments

--- src/View.hs
+++ src/View.hs
@@ -32,9 +32,9 @@ drawState (World gs tool colour) =
 --
 -- >>> drawNewGraphic (World [] (RectangleTool (Just (-3, 3))) Black) (Just (3, -3))
 -- World [Graphic (Rectangle 6.0 6.0) Black (0.0,0.0)] (RectangleTool Nothing) Black
--- 
+--
 -- | drawNewGraphic Test 2
--- 
+--
 -- >>> drawNewGraphic (World [] (RectangleTool (Just (0, 0))) Orange) (Just (3, 7))
 -- World [Graphic (Rectangle 3.0 7.0) Orange (1.5,3.5)] (RectangleTool Nothing) Orange
 drawNewGraphic :: State -> Maybe Point -> State
@@ -43,6 +43,20 @@ drawNewGraphic = undefined -- TODO
 getNewGraphic :: State -> Maybe Point -> Maybe Graphic
 getNewGraphic = undefined -- TODO
 
+-- JK: You are repeating yourself a lot here. A `where` clause would
+-- simplify things a lot (or even pulling the common computations into
+-- `getWidthHeightShift`).
+--
+-- Note that the marking scheme for Part A only marks
+-- functionality. When I mark the full assignment, it will consider
+-- style too.
+--
+-- Also, your calculations aren't quite right. Consider a
+-- rectangle. `shapeToPic` will drop it with its midpoint the origin
+-- (0, 0). The shift you want will move the midpoint of the rectangle
+-- to (mid x1 x2, mid y1 y2), where mid is a function to calculate
+-- halfway between its two arguments. What does that function look
+-- like?
 getRectangleGraphic :: Point -> Point -> ColourName -> Graphic
 getRectangleGraphic (x1,y1) (x2,y2) c= Graphic (Rectangle (abs(x2-x1))(abs(y2-y1))) c (x2-abs(x2-x1)/2,y2-abs(y2-y1)/2)
 
